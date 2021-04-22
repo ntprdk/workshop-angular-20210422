d92cd7b0f7d887bc4a3a03ea24d2d3acf0455aed
sovernut <orenomail4@gmail.com>
2021-04-23 00:19:15 +0700

    twinkle twinkle little star

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,4 +1,8 @@
 export class User {
-    constructor(public id: number, public firstName: string, public lastName: string, public email: string) {
+    constructor(public id: number,
+      public firstName: string,
+      public lastName: string,
+      public email: string,
+      public score: number ) {
     }
   }
---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent implements OnInit {
+  @Input() targetScore = 0;
+  starWidth = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.starWidth = this.targetScore * 75 / 5;
+  }
+
+}
