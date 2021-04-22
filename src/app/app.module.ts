db2a57fbe6eb221298e04f9f0f53335c482357d5
nuttaphon_r <nuttaphon.r@kbtg.tech>
2021-04-22 14:32:58 +0700

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MyUpperCasePipe } from './my-upper-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MyUpperCasePipe,
   ],
   imports: [
     BrowserModule,
