deedb94eadb4fd8220b4100224268c219b874666
nuttaphon_r <nuttaphon.r@kbtg.tech>
2021-04-22 14:21:28 +0700

    ngFor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  username = ""
-  password = ""
-  constructor() { }
+  users: User[] = [];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.users = [
+      new User(1, 'user 01', 'last 01', 'xxx@demo1.com'),
+      new User(2, 'user 02', 'last 02', 'xxx@demo2.com'),
+      new User(3, 'user 03', 'last 03', 'xxx@demo3.com'),
+    ];
   }
 
 }
