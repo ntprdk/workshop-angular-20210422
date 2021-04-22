12fcb44c8f7946bfc976bad46580382a47a20459
nuttaphon_r <nuttaphon.r@kbtg.tech>
2021-04-22 12:14:29 +0700

    login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'demo01';
   result = "...";
+  username = "";
 
   onPress(event: any){
     console.log(event.target.value)
     this.result = event.target.value
+  }
+  doSth(){
+    console.log("do something")
+    this.result = "change from click"
   }
 }
